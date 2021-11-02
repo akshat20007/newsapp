253a6999afbbb6a17d22cc171d56416231fca291
Akshat Bahukhandi <akshat.b2007@gmail.com>
2021-11-03 01:47:09 +0530

    update function not working properly

--- src/App.js
+++ src/App.js
@@ -3,13 +3,48 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
 
 export default class App extends Component {
+  pageSize=15;
   render() {
     return (
-      <div style={{backgroundColor:'black'}}>
-        <Navbar/>
-        <News></News>
+      <div >
+        <Router>
+        <Navbar/>       
+        <Switch>
+          <Route exact path="/">
+          <News pageSize={this.pageSize} key='general' country='in' category='general'/>
+          </Route>
+          <Route exact path="/business">
+          <News pageSize={this.pageSize} key='business' country='in' category='business'/>
+          </Route>
+          <Route exact path="/entertainment">
+          <News pageSize={this.pageSize} key='entertainment' country='in' category='entertainment'/>
+          </Route>
+          <Route exact path="/general">
+          <News pageSize={this.pageSize} key='general' country='in' category='general'/>
+          </Route>
+          <Route exact path="/health">
+          <News pageSize={this.pageSize} key='health' country='in' category='health'/>
+          </Route>
+          <Route exact path="/science">
+          <News pageSize={this.pageSize} key='science' country='in' category='science'/>
+          </Route>
+          <Route exact path="/sports">
+          <News pageSize={this.pageSize} key='sports' country='in' category='sports'/>
+          </Route>
+          <Route exact path="/technology">
+          <News pageSize={this.pageSize} key='technology' country='in' category='technology'/>
+          </Route>
+                   
+        </Switch>
+        </Router>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 
 export class NewsItem extends Component {
-    
   render() {
-     let {title, description, imageUrl, newsUrl }= this.props
+    let { title, description, imageUrl, newsUrl, author, time,outlate } = this.props;
     return (
-      <div>
-        <div className="card" style={{width: '18rem'}}>
-          <img src={imageUrl===null?'https://english.cdn.zeenews.com/sites/default/files/2021/10/29/983429-solar-storm-nasa.jpg':imageUrl} className="card-img-top" alt="..." />
+      <div className="my-3">
+        <div className="card">
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{zIndex:1, left:'90%'}}>{outlate}
+          </span>
+          <img
+            src={
+              imageUrl === null
+                ? "https://english.cdn.zeenews.com/sites/default/files/2021/10/29/983429-solar-storm-nasa.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
             <p className="card-text">
-              {description}...
+              <small className="text-muted">
+                By {author} On {new Date(time).toGMTString()}{" "}
+              </small>
             </p>
-            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-dark btn-sm">
-              Go somewhere
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-dark btn-sm"
+            >
+              Read More
             </a>
           </div>
         </div>
